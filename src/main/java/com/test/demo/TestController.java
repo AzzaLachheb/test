2b5468520b88429b92784621569499c982ded9fb
Azza Lachheb <azzalachheb@gmail.com>
2021-04-01 19:19:08 +0100

    commit

--- src/main/java/com/test/demo/TestController.java
+++ src/main/java/com/test/demo/TestController.java
@@ -1,21 +1,26 @@
 package com.test.demo;
 
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
-	@GetMapping("/test")
-	public String greeting(@RequestParam(value = "nbr", defaultValue = "1") int nbr) {
+	static final String CONST = "response";
+	@GetMapping(value = "/test", 
+    produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map greeting(@RequestParam(value = "nbr", defaultValue = "1") int nbr) {
 		if ((nbr % 3 == 0) && (nbr % 5 == 0))
-			return "FizzBuzz";
+			return Collections.singletonMap(CONST,"FizzBuzz");
 		else if (nbr % 5 == 0)
-			return "Buzz";
+			return Collections.singletonMap(CONST,"Buzz");
 		else if (nbr % 3 == 0)
-			return "Fizz";
+			return Collections.singletonMap(CONST,"Fizz");
 		else
-			return nbr + 1 + "";
+			return Collections.singletonMap(CONST,nbr + 1 + "");
 	}
 }
